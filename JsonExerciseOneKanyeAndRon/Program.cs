fcc2ac8fb1204c1c4446397022b71e44160cfed4
Baruc Gomez <113378032+Bdotgo17@users.noreply.github.com>
2022-10-01 14:26:43 -0500

    initial commit

---
+++ JsonExerciseOneKanyeAndRon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace JsonExerciseOneKanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
+
